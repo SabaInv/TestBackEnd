ceb72c2f553e02ee78c1fa86fc79665639aa2083
Saba <sabaliauskaspablo@gmail.com>
2023-03-10 06:11:42 -0500

    fix

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send({
+        "hello"
+  });
+  });
+
 app.get('/mobile-app/v1/validate-domain', (req, res) => {
   res.send({
     name: 'Invgate',
     logo: 'https://upload.wikimedia.org/wikipedia/commons/f/f4/BMW_logo_%28gray%29.svg',
-    url:'localhost:3000'
+    url:'https://invgate-test.herokuapp.com'
 });
 });
 
